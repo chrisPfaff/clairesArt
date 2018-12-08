889c9bef1ea6c8ec005cbb72f8e16018ae4394a2
Chris Pfaff <chrispfaff10@gmail.com>
2018-12-08 14:55:00 -0600

    vanilla js art display

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const port = 8888;
+
+const app = express();
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.listen(port, () => {
+  console.log("server is listening");
+});
